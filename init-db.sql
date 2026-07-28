2b355aad670be23ba68019056e1b322741078bd7
didier defrance <didier@d-defrance.fr>
2026-07-28 11:10:15 +0200

    suite

--- init-db.sql
+++ init-db.sql
@@ -1,6 +1,7 @@
-CREATE DATABASE geoDB IF NOT EXISTS;
-CREATE TABLE departement(
-	num integer auto_increment NOT NULL,
-	nom VARCHAR(64),
-	prefecture VARCHAR(64),
-	PRIMARY KEY(num));
+show database;
+#CREATE DATABASE geoDB IF NOT EXISTS;
+#CREATE TABLE departement(
+#	num integer auto_increment NOT NULL,
+#	nom VARCHAR(64),
+#	prefecture VARCHAR(64),
+#	PRIMARY KEY(num));
